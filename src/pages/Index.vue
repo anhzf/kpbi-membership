043f3633e0b7c43e0d5826e179f047e7e819eccc
Anhzf <36941633+anhzf@users.noreply.github.com>
2021-08-29 13:34:08 +0700

    feat(page): :sparkles: load image for profile details at Index.vue

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -32,6 +32,8 @@
   <q-dialog v-model="isDialogOpen">
     <card-profile
       :data="currentCardProfile"
+      :pt-img-src="pTPhotoUrl"
+      :kaprodi-img-src="kaprodiPhotoUrl"
       class="profile-card q-py-md"
     />
   </q-dialog>
@@ -41,27 +43,44 @@
 import {
   defineComponent, reactive, computed, toRefs,
 } from 'vue';
-import { useAsyncState } from '@vueuse/core';
+import { useAsyncState, asyncComputed } from '@vueuse/core';
 import ListTableMember from 'src/components/ListTableMember.vue';
-import { useCollection } from 'src/use/firestore';
-import { collections, jsonDateToTimestamp, memberMergeDefaults } from 'src/firestoreServices';
 import CardProfile from 'src/components/CardProfile.vue';
+import { useCollection } from 'src/use/firestore';
+import {
+  collections, jsonDateToTimestamp, memberMergeDefaults,
+} from 'src/firestoreServices';
+import ProfileUseCase from 'src/useCases/profile';
+import { promiseProxy } from 'src/helpers';
 import type { Member, Model, ModelInObject } from 'app/common/schema';
 import type { LegacyProfile } from 'app/common/legacy';
-import ProfileUseCase from 'src/useCases/profile';
+
+type ModelObject = ModelInObject<Model<Member>> & ({
+  _isLegacy: true;
+  _kaprodiImgSrc: string;
+  _pTImgSrc: string;
+} | {
+  _isLegacy?: false;
+});
 
 export default defineComponent({
   name: 'PageIndex',
-
   components: { ListTableMember, CardProfile },
-
   setup() {
     const state = reactive({
       search: '',
-      currentCardProfile: null as (Member | null),
+      currentCardProfile: null as (ModelObject | null),
       isDialogOpen: false,
     });
     const { data, isLoading } = useCollection(collections.Members);
+    const profileUseCase = computed(() => {
+      if (state.currentCardProfile) {
+        const docRef = collections.Members.doc(state.currentCardProfile._uid);
+        return new ProfileUseCase(docRef);
+      }
+
+      return null;
+    });
     const { state: oldData, isReady } = useAsyncState(async () => {
       const res = await fetch('https://anggota.kpbi.or.id/api/kpbi/profiles');
       const profiles = await res.json() as LegacyProfile[];
@@ -72,23 +91,33 @@ export default defineComponent({
         _created: jsonDateToTimestamp(profile.created_at),
         _deleted: null,
         ...ProfileUseCase.legacyProfileConverter(profile),
-      }) as ModelInObject<Model<Member>>);
+      }) as ModelObject);
     }, []);
-    const mergedData = computed<ModelInObject<Model<Member>>[]>(() => [
+    const mergedData = computed<ModelObject[]>(() => [
       ...data.value,
       ...oldData.value.filter((el) => data.value
         .findIndex((newEl) => newEl.emailProdi === el.emailProdi)),
     ]);
+    const pTPhotoUrl = asyncComputed(promiseProxy(() => Promise
+      .resolve(state.currentCardProfile?._isLegacy
+        ? `https://anggota.kpbi.or.id/${state.currentCardProfile._pTImgSrc}`
+        : profileUseCase.value?.getPTPhoto())), undefined);
+    const kaprodiPhotoUrl = asyncComputed(promiseProxy(() => Promise
+      .resolve(state.currentCardProfile?._isLegacy
+        ? `https://anggota.kpbi.or.id/${state.currentCardProfile._kaprodiImgSrc}`
+        : profileUseCase.value?.getKaprodiPhoto())), undefined);
 
     return {
       ...toRefs(state),
       data: mergedData,
+      pTPhotoUrl,
+      kaprodiPhotoUrl,
       isLoading: computed(() => isLoading.value && isReady.value),
     };
   },
   methods: {
-    onDetailClick(member: Partial<Member>) {
-      this.currentCardProfile = memberMergeDefaults(member);
+    onDetailClick(member: Partial<ModelObject>) {
+      this.currentCardProfile = memberMergeDefaults(member) as ModelObject;
       this.isDialogOpen = true;
     },
   },
