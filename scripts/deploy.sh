48991e1823fb78f6de0c9c86bdfb4daabe2c3acd
Anhzf <me@anhzf.dev>
2024-03-14 16:54:12 +0700

    ci: :construction_worker: Set output to artifact only

---
+++ scripts/deploy.sh
@@ -0,0 +1,25 @@
+# !/bin/bash
+
+REPO="anhzf/kpbi-membership"
+ARTIFACT=$1
+
+# Set GH_TOKEN via env variables
+# Abort script when no GH_TOKEN
+if [ -z $GH_TOKEN ]; then
+  echo "GH_TOKEN is not set. Aborting."
+  exit 1
+fi
+
+if [ -z $ARTIFACT ]; then
+  echo "ARTIFACT is not set. Aborting."
+  exit 1
+fi
+
+curl -L \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer $GH_TOKEN" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/repos/$REPO/actions/artifacts/$ARTIFACT/zip \
+  -o dist.zip
+
+unzip dist.zip -d www
